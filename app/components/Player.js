76121ab9cb83691209a22cf38092012017fb8f4e
Andrew Tran <andrew.tran345@gmail.com>
2017-01-21 05:04:02 -0800

    Added components/Player.css, Player.js; edited PlayerPage.js with TODOs

---
+++ app/components/Player.js
@@ -0,0 +1,10 @@
+//@flow
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+import styles from './Player.css';
+
+class Player extends React.Component {
+  props: {
+    havingFun: boolean
+  };
+}
--- app/containers/PlayerPage.js
+++ app/containers/PlayerPage.js
@@ -1,2 +1,6 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import Player from '../components/Player'
+// import actions
+
+// Needs mapStateToProps(state) and mapDispatchToProps(dispatch)
