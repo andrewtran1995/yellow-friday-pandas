a4348d535411d77e570be5e1631cd53e2be36683
Yamn <mistayam94@gmail.com>
2017-01-21 09:18:29 -0800

    p5 library and script for buttons, etc.

---
+++ music.js
@@ -0,0 +1,28 @@
+var song;
+
+function setup() {
+	song = loadSound('03. 미쳤나봐.mp3');
+	createCanvas(720, 200);
+	background(255,0,0);
+}
+
+function playPressed() {
+	if (!song.isPlaying()) { // .isPlaying() returns a boolean
+		song.play();
+		background(0, 255, 0);
+	}
+}
+
+function stopPressed() {
+	if (song.isPlaying()) { // .isPlaying() returns a boolean
+		song.stop();
+		background(255, 0, 0);
+	}
+}
+
+function pausePressed() {
+	if ( song.isPlaying() ) { // .isPlaying() returns a boolean
+		song.pause();
+		background(255, 255, 0);
+	}
+}
