33955246f4fa2fd4d56318ecb8d4c1b6084dd473
Andrew Tran <andrew.tran345@gmail.com>
2017-01-21 19:50:58 -0800

    Aaron's changes

--- visualization.js
+++ visualization.js
@@ -4,7 +4,7 @@ var VISUALIZATIONS = new Map();
 
 BarVisualization = {
 	onRender: function() {
-		createCanvas(720, 200);
+		createCanvas(720, 400);
 		background(173,216,230);
 	},
 	onPlay: function() {
@@ -19,13 +19,14 @@ BarVisualization = {
 	},
 	draw: function(fft) {
 		background(173,216,230);
-		stroke(255);
-		fill(0); // spectrum is green
+
+		fill(255); // spectrum is green
+		noStroke();
 		var spectrum = fft.analyze();
 		for (var i = 0; i< spectrum.length; i++){
 			var x = map(i, 0, spectrum.length, 0, width);
 			var h = -height + map(spectrum[i], 0, 255, height, 0);
-			rect(x, height, width * 2 / spectrum.length, h )
+			rect(x, height, width / spectrum.length, h )
 		}
 	}
 }
@@ -52,7 +53,7 @@ EnvelopeVisualization = {
 		// plot FFT.analyze() frequency analysis on the canvas
 		var spectrum = fft.analyze();
 		for (var i = 0; i < spectrum.length/20; i++) {
-			fill(spectrum[i], spectrum[i]/10, 0);
+			fill(spectrum[i]/10, spectrum[i], 0);
 			var x = map(i, 0, spectrum.length/20, 0, width);
 			var h = map(spectrum[i], 0, 255, 0, height);
 			rect(x, height, spectrum.length/20, -h);
