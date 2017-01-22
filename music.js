68a7094ef5ac5bcd52011e044682feed9a177b89
Andrew Tran <andrew.tran345@gmail.com>
2017-01-21 21:26:38 -0800

    Ensure fft input is song

--- music.js
+++ music.js
@@ -14,6 +14,7 @@ function setup(audioFilePath) {
 	song = loadSound(audioFilePath);
 	VISUALIZATIONS.get(visualization).onRender();
 	fft = new p5.FFT();
+	fft.setInput(song);
 }
 
 function draw() {
